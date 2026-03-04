9128a843e06093b6a71a8b10dc57d8a00a020add
Jojo <e2566476@cmaisonneuve.qc.ca>
2026-03-04 15:25:16 -0500

    Update Skieur.cs

--- Assets/Scripts/Skieur.cs
+++ Assets/Scripts/Skieur.cs
@@ -1,22 +1,28 @@
 using UnityEngine;
 using UnityEngine.Animations;
-
+using UnityEngine.InputSystem;
 public class Skieur : MonoBehaviour
 {
+    public InputAction mouvHoriz;
+    public InputAction mouvVert;
+    public float vitesse = 3f;
+
     void Start()
     {
-
+       
     }
 
     // Utiliser ces fonctions pour activer et désactiver les InputActions
     private void OnEnable()
     {
-
+        mouvHoriz.Enable();
+        mouvVert.Enable();
     }
 
     private void OnDisable()
     {
-
+        mouvHoriz.Disable();
+        mouvVert.Disable();
     }
 
 
